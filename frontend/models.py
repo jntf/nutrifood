2dbc215452da521ddbc492089541965960cc157a
julien <107534950+jntf@users.noreply.github.com>
2023-03-20 12:32:47 +0100

    Exercice 4

--- frontend/models.py
+++ frontend/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Question(models.Model):
+class Produit(models.Model):
     ingredient = models.CharField(max_length=255)
     created_at = models.DateTimeField('created_at')
